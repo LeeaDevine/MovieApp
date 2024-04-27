c1cc5654fe25f4b67ff3f9a69fb1f8a6267f871c
Lee Devine <94686562+LeeaDevine@users.noreply.github.com>
2024-04-27 16:23:25 +0100

    Initial Commit + Nav[basic]
    Create project.
    Added basic Nav
    
    Added some components [movie/]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,39 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MovieSearchComponent } from './movie/components/movie-search/movie-search.component';
+import { MovieListComponent } from './movie/components/movie-list/movie-list.component';
+import { MovieDetailsComponent } from './movie/components/movie-details/movie-details.component';
+import { MovieItemComponent } from './movie/components/movie-item/movie-item.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { NavComponent } from './nav/nav.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieSearchComponent,
+    MovieListComponent,
+    MovieDetailsComponent,
+    MovieItemComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
